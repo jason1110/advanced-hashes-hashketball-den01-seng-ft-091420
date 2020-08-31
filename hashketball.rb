d56597c6761d71520ec6e78517f3e6073493d0bd
Jason Easterly <jason.easterly11@gmail.com>
2020-08-31 19:42:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -276,11 +276,9 @@ def long_name_steals_a_ton?
     most_steals = value[:players].max_by {|steals| steals[:steals]}[:player_name]
   end
   if player_with_longest_name == most_steals
-    "#{player_with_longest_name} steals the most"
+    p "#{player_with_longest_name} steals the most"
   else
-    "Someone else steals a lot"
+    p "Someone else steals a lot"
   end  
 end  
 
-
-long_name_steals_a_ton?
