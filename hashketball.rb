150c83e7f2faaa6bbbf26b966a4703e70c2a1a7b
Jason Easterly <jason.easterly11@gmail.com>
2020-08-31 19:15:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -251,7 +251,7 @@ game_hash.each do |key, value|
 end
 end
 home_team total = home_team_array.reduce(0) {|total, num| total + num}
-away_team_array.reduce(0) {|total, num| total + num}
+away_team_total = away_team_array.reduce(0) {|total, num| total + num}
 binding.pry
 end
 
